fb96e861aac21e1cec5fb14f4a64aedaa0f835fe
Vytautas <vytautasragaisis07@gmail.com>
2020-01-19 20:49:11 +0200

    commit

--- Ketvirta/view/index.view.php
+++ Ketvirta/view/index.view.php
@@ -6,60 +6,85 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="view/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title><?=siteName;?></title>
+    <title><?=siteTitle;?></title>
 <body>
+<?php
+$skrydziaiis = ['Roma','Briuselis','Londonas', 'Oslas', 'Ženeva'];
+$skrydziaii = ['Berlynas','Miunchenas','Jeruzalė', 'Michiganas', 'Vilnius'];
+$skrydzioNr = ['1000', '1001', '1002', '1003', '1004', '1005', '1006', '1007', '1008'];
+$bagazas = [10, 15, 20, 25, 30];
+$validation_errors=[];
+if(isset($_POST['submit'])) {
+    if (!isset($_POST['flyNr'])) {
+        $validation_errors[] = "Skrydžio numeris turi būti įvestas!";
+    }
+    if (!isset($_POST['nameId'])) {
+        $validation_errors[] = "Asmens kodas turi būti įvestas!";
+    }
+}
+$flyNr = $_POST['flyNr'];
+$nameId = $_POST['nameId'];
+$firstName = $_POST['firstName'];
+$isKur = $_POST['isKur'];
+$iKur = $_POST['iKur'];
+$kaina = $_POST['kaina'];
+$svoris = intval($_POST['bagazas']);
+
+$bagazoKaina = intval($_POST['kaina']);
+if ($svoris >= 20) {
+    $bagKaina = 30;
+    $kainuoja = $kaina + 30;
+} else {
+    $bagKaina = 0;
+    $kainuoja = $kaina;
+}
+
+?>
+<?php if($validation_errors) :?>
+    <div class="errors">
+        <ul>
+            <?php foreach($validation_errors as $error) :?>
+                <li><?=$error;?></li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+
+<?php endif; ?>
 <div class="container">
     <h1><?=siteName;?></h1>
-    <table>
-    <?php
-    $skrydziaiis = ['Roma','Briuselis','Londonas', 'Oslas', 'Ženeva'];
-    $skrydziaii = ['Berlynas','Miunchenas','Jeruzalė', 'Michiganas', 'Vilnius'];
-    $skrydzionr = ['1000', '1001', '1002', '1003', '1004', '1005', '1006', '1007', '1008'];
-    $bagazas = ['10', '15', '20', '25', '30'];
-    ?>
-    <?php
-    if(isset($_POST['submit'])) {
-        foreach ($_POST as $value) {
-        }
-            $price = $_POST('kaina');
-            $weight = $_POST('bagazai');
-            $weightPrice = 0;
-    }
-    ?>
-    </table>
-    <form action = "<?php $_PHP_SELF ?>" method = "POST">
+    <form method="POST">
         <div class="form-group">
             <label for="formGroupExampleInput2">Skrydžio nr:</label>
-            <select name="flynr" id="flynr" class="form-control">
+            <select name="flyNr" class="form-control">
                 <option selected disabled>--Pasirinkite--</option>
-                <?php foreach ($skrydzionr as $skrydzioid):?>
-                    <option value="<?=$skrydzioid?>"><?=$skrydzioid?></option>
+                <?php foreach ($skrydzioNr as $skrydzioId):?>
+                    <option value="<?=$skrydzioId;?>"><?=$skrydzioId;?></option>
                 <?php endforeach;?>
             </select>
         </div>
         <div class="form-group">
             <label for="formGroupExampleInput2">Asmens kodas</label>
-            <input type="number" class="form-control" id="nameid" name="nameid" placeholder="...">
+            <input type="number" class="form-control" name="nameId" placeholder="...">
         </div>
         <div class="form-group">
             <label for="formGroupExampleInput2">Vardas ir pavardė</label>
-            <input type="text" class="form-control" id="firstname" name="firstname" placeholder="...">
+            <input type="text" class="form-control" id="firstName" name="firstName" placeholder="...">
         </div>
         <div class="form-group">
             <label for="formGroupExampleInput2">Skrydis iš:</label>
-            <select name="skrydisis" id="skrydisis" class="form-control">
+            <select name="isKur" id="isKur" class="form-control">
                 <option selected disabled>--Pasirinkite--</option>
                 <?php foreach ($skrydziaiis as $flyFrom):?>
-                    <option value="<?=$flyFrom?>"><?=$flyFrom?></option>
+                    <option value="<?=$flyFrom;?>"><?=$flyFrom;?></option>
                 <?php endforeach;?>
             </select>
         </div>
         <div class="form-group">
             <label for="formGroupExampleInput2">Skrydis į:</label>
-            <select name="skrydisi" id="skrydisi" class="form-control">
+            <select name="iKur" id="iKur" class="form-control">
                 <option selected disabled>--Pasirinkite--</option>
                 <?php foreach ($skrydziaii as $flyIn):?>
-                    <option value="<?=$flyIn?>"><?=$flyIn?></option>
+                    <option value="<?=$flyIn;?>"><?=$flyIn;?></option>
                 <?php endforeach;?>
             </select>
         </div>
@@ -69,10 +94,10 @@
         </div>
         <div class="form-group">
             <label for="formGroupExampleInput2">Bagažas:</label>
-            <select name="bagazas" id="bagazas" class="form-control">
+            <select name="bagazas" class="form-control">
                 <option selected disabled>--Pasirinkite--</option>
                 <?php foreach ($bagazas as $bagazai):?>
-                    <option value="<?=$bagazai?>"><?=$bagazai?></option>
+                    <option value="<?=$bagazai;?>"><?=$bagazai;?></option>
                 <?php endforeach;?>
             </select>
         </div>
@@ -87,33 +112,23 @@
         </div>
         <div class="row ticket">
             <div class="row ticket mainInfo">
-                <div class="col-sm-3">Skrydžio Numeris<p></p></div>
-                <div class="col-sm-3">Skrydis pirmyn<p></p></div>
-                <div class="col-sm-3">Skrydis atgal
-                    <?php
-                    if( $_POST["isKur"]) {
-                        if (preg_match("/[^A-Za-z'-]/",$_POST['isKur'] )) {
-                            die ("invalid name and name should be alpha");
-                        }
-                        echo "Welcome ". $_POST['isKur']. "<br />";
-
-                        exit();
-                    }
-                    ?></div>
+                <div class="col-sm-3">Skrydžio Numeris<p><?=$flyNr;?></p></div>
+                <div class="col-sm-3">Skrydis pirmyn<p><?=$isKur?></p></div>
+                <div class="col-sm-3">Skrydis atgal<p><?=$iKur?></p></div>
             </div>
 
             <div class="row personal">
                 <div class="row personal mainInfo">
-                    <div class="col-lg-8">Vardas ir Pavardė<p></p></div>
-                    <div class="col-lg-8">Asmens kodas<p></p></div>
+                    <div class="col-lg-8">Vardas ir Pavardė<p><?=$firstName;?></p></div>
+                    <div class="col-lg-8">Asmens kodas<p><?=$nameId;?></p></div>
                 </div>
             </div>
 
             <div class="col price">
                 <div class="row price mainInfo">
-                    <div class="col-sm-3">Bilieto kaina<?=$isKur?></div>
-                    <div class="col-sm-3">Bagažo kaina<p></p></div>
-                    <div class="col-sm-3">Galutinė kaina<p></p></div>
+                    <div class="col-sm-3">Bilieto kaina<p><?=$kaina?>€‎</p></div>
+                    <div class="col-sm-3">Bagažo kaina<p><?=$bagKaina; ?>€‎</p></div>
+                    <div class="col-sm-3">Galutinė kaina<p><?=$kainuoja; ?>€‎</p></div>
                 </div>
             </div>
         </div>
